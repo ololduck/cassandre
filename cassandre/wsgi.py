90952a3336cfccc4ec5b77f4990bb7f49ad69932
Paul Ollivier <paul.ollivier.contact@gmail.com>
2012-08-16 23:03:11 +0200

    fix bug module

--- cassandre/wsgi.py
+++ cassandre/wsgi.py
@@ -15,7 +15,8 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cassandre.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cassandre.settings.dev")
+print(os.environ)
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
