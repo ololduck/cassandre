4af24cf3f4db86dfd18f13fbe0812bfbfbb40d0a
Paul Ollivier <paul.ollivier.contact@gmail.com>
2012-08-16 18:42:03 +0200

    Added envvars for DB

--- cassandre/settings/prod.py
+++ cassandre/settings/prod.py
@@ -14,11 +14,11 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'NAME': 'dfhre4ce6olr2r',  # Or path to database file if using sqlite3.
+        'USER': os.environ['PG_LOGIN'],  # Not used with sqlite3.
+        'PASSWORD': os.environ['PG_PASSWD'],  # Not used with sqlite3.
+        'HOST': 'ec2-107-22-164-195.compute-1.amazonaws.com',  # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '5432',  # Set to empty string for default. Not used with sqlite3.
     }
 }
 
