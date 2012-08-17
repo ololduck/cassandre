a57426109dca5fccdafe4bfbb34bfc45a7e27356
Paul Ollivier <paul.ollivier.contact@gmail.com>
2012-08-17 08:38:53 +0200

    Added urls routes, changed models, and settings

--- cassandre/cassandre/models.py
+++ cassandre/cassandre/models.py
@@ -2,12 +2,9 @@ from django.db import models
 
 
 class Micropost(models.Model):
-    class Meta:
-        verbose_name = _('Micropost')
-        verbose_name_plural = _('Microposts')
 
     content = models.CharField(max_length=160)
-    author = models.ForeignKey(User)
+    # author = models.ForeignKey(User)
     date = models.DateField()
 
     def __unicode__(self):
