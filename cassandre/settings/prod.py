6b73c996931987157458cc6bcb9416327bf4856b
Paul Ollivier <paul.ollivier.contact@gmail.com>
2012-08-17 19:40:49 +0200

    Changed procfile

--- cassandre/settings/prod.py
+++ cassandre/settings/prod.py
@@ -125,7 +125,8 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'cassandre.cassandre',
-    'tastypie'
+    'tastypie',
+    'gunicorn',
 )
 
 # A sample logging configuration. The only tangible logging
