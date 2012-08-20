230b6dc3574417361ce0a4d7c007e2251e5b1054
Paul Ollivier <paul.ollivier.contact@gmail.com>
2012-08-20 20:45:24 +0200

    Changed urls to have a landing page

--- cassandre/client/urls.py
+++ cassandre/client/urls.py
@@ -1,5 +1,5 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-    url(r'', 'cassandre.client.views.home'),
+    url(r'^$', 'cassandre.client.views.home'),
 )
