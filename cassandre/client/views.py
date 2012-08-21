b6fb16d88681cd6498b5b408379f6c2261c9aed0
Paul Ollivier <paul.ollivier.contact@gmail.com>
2012-08-21 09:29:36 +0200

    début d'UI

--- cassandre/client/views.py
+++ cassandre/client/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render_to_response
 
 
 def home(request):
-    if(request.user.is_authentificated()):
-        return render_to_response('login.html')
+    if(request.user.is_authenticated()):
+        u = request.user
+        return render_to_response('home.html', {
+            'posts': u.microposts.all()
+            })
     else:
-        return render_to_response('home.html')
+        return render_to_response('login.html')
