c6eaa4a5fc66c122c2ce9b40f274b58dc0d428a6
Paul Ollivier/SDDV6068 <paul.ollivier@orange.com>
2012-08-21 15:38:09 +0200

    Cleaned up views code

--- cassandre/client/views.py
+++ cassandre/client/views.py
@@ -4,15 +4,12 @@ from django.contrib.auth import authenticate, login
 
 
 def home(request):
-    print(request.user.is_authenticated())
     if request.user.is_authenticated():
-        print("is auth")
         u = request.user
         return render_to_response('home.html', {
             'posts': u.microposts.all()
             })
     else:
-        print("Not authenticated")
         from django.core.context_processors import csrf
         c = {}
         c.update(csrf(request))
@@ -21,13 +18,11 @@ def home(request):
 
 def loginView(request):
     if request.user.is_authenticated():
-        print("User already auth")
         return HttpResponseRedirect('/')
     else:
         print("user not auth")
         user = authenticate(username=request.POST['login'], password=request.POST['password'])
         if user is not None:
-            print("Hello, user, welcome back!")
             login(request, user)
             return HttpResponseRedirect('/')
         else:
